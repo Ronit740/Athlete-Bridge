464b5541e34a54f278dd57cae826fcc3c35ef79a
Ronit Mathur <ronit.oct11@gmail.com>
2025-02-08 16:56:28 +0530

    trying to merge

--- src/pages/home.js
+++ src/pages/home.js
@@ -3,6 +3,7 @@ function Home() {
         <div>
             <h2>Welcome to Athlete Bridge</h2>
             <p>Your one-stop platform for athlete support.</p>
+            <p>This is the reels feature</p>
         </div>
     );
 }
